3dc627ee73d3de70addb54e4b4d16f907a13a2d9
alexpohorecky <alexanderpohorecky@gmail.com>
2018-02-07 15:20:31 -0600

    Added Basic Interaction

---
+++ basicInteractionTest/script.js
@@ -0,0 +1,20 @@
+let pressedKey;
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+
+}
+
+function keyPressed(){
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
+  ellipse(random(0, width), random(0, height), random(100), random(100));
+}
+
+function mousePressed(){
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
+  rect(mouseX, mouseY, random(100), random(100));
+}
