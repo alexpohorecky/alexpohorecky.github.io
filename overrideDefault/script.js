f40556273fb1ff5590b4e7f1c6c6a5fea68e7eac
alexpohorecky <alexanderpohorecky@gmail.com>
2018-02-12 14:33:37 -0600

    override default browser test

---
+++ overrideDefault/script.js
@@ -0,0 +1,27 @@
+let lightSwitch;
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  lightSwitch = true;
+}
+
+function draw(){
+  drawLight();
+}
+
+function keyTyped(){
+  if (key === " "){
+    lightSwitch = !lightSwitch;
+    return false;
+  }
+}
+function drawLight(){
+  if (lightSwitch){
+    fill(255);
+  }
+  else{
+    fill(0);
+  }
+  rectMode(CENTER)
+  rect(width/2, height/2, 100, 100);
+}
