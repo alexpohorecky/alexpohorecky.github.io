af0aab099c6c0b30e935b0fd9b6304f0eeddbe0d
alexpohorecky <alexanderpohorecky@gmail.com>
2018-02-13 12:17:47 -0600

    vector test

--- vectorTest/sketch.js
+++ vectorTest/sketch.js
@@ -3,5 +3,15 @@ function setup(){
 }
 
 function draw(){
+  Ball(100, 100, 20);
+}
+
+function Ball(x, y, r){
+  this.pos = createVector(x,y);
+  this.vel = createVector(1,0);
+  this.acc = createVector(1,0);
+  this.pos.add(this.vel);
+  this.vel.add(this.acc);
+  ellipse(x, y, r);
 
 }
