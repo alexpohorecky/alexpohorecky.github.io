54031cf337873f034bd74a169fa2c74008dcc30b
ComorantPostman <alexanderpohorecky@gmail.com>
2018-02-13 21:49:48 -0600

    created the ball as an object

--- vectorTest/sketch.js
+++ vectorTest/sketch.js
@@ -1,26 +1,44 @@
+let ball;
+let ball1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  ball = new Ball(0, 0, 20, 10, 10, 0, 0);
+  //ball1 = new Ball(200, 100, 20, 10, 0, -1, 0);
 
 }
 
 function draw() {
-  //background(255);
-  ball = new Ball(100, 100, 20);
   ball.move();
-//  ball.render();
+  //ball1.move();
+  ball.animate();
+  //ball1.animate();
 }
 
-function Ball(x, y, r) {
-  this.pos = createVector(x, y);
-  this.vel = createVector(5, 5);
-  this.acc = createVector(5, 5);
-  this.move = function(){
-    this.pos.add(this.vel);
-    this.vel.add(this.acc);
-    ellipse(this.pos.x, this.pos.y, r);
+class Ball {
+  constructor(_x, _y, _r, xVelocity, yVelocity, xAcceleration, yAcceleration) {
+    this.x = _x;
+    this.y = _y;
+    this.r = _r;
+
+    this.pos = createVector(this.x, this.y);
+    this.vel = createVector(xVelocity, yVelocity);
+    this.accel = createVector(xAcceleration, yAcceleration);
   }
-  // this.render = function(){
-  //   ellipse(this.pos.x, this.pos.y, r);
-  // }
+  move() {
+
+    if (this.pos.x > width || this.pos.x < 0){
+      this.vel.x *= -1;
+      //this.accel.x *= 0.1;
+    }
+    if (this.pos.y > height || this.pos.y < 0){
+      this.vel.y *= -1;
+      //this.accel.y *= 0.1;
+    }
+    this.pos.add(this.vel);
+    this.vel.add(this.accel);
+  }
+  animate() {
+    ellipse(this.pos.x, this.pos.y, this.r);
+  }
 }
