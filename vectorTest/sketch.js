0401e9f711e107dbd0356c01ffd4c0953a6df0a6
alexpohorecky <alexanderpohorecky@gmail.com>
2018-02-15 15:21:36 -0600

    fixed bounce

--- vectorTest/sketch.js
+++ vectorTest/sketch.js
@@ -1,21 +1,27 @@
 let ball;
 let ball1;
-
+ballExists = false;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ball = new Ball(100, 100, 20, 0, 0, 0, 0.98);
+  //ball = new Ball(100, 500, 20, 10, 0, 0, 1);
   //ball1 = new Ball(200, 100, 20, 10, 0, -1, 0);
 
 }
 
 function draw() {
-  background(255);
+  //background(255);
+  if (ballExists){
   ball.move();
+  print(ball.vel.y)
   //ball1.move();
   ball.animate();
   //ball1.animate();
 }
-
+}
+function mouseClicked(){
+  ballExists = true;
+  ball = new Ball(mouseX, mouseY, 20, 10, 0, 0, 1);
+}
 class Ball {
   constructor(_x, _y, _r, xVelocity, yVelocity, xAcceleration, yAcceleration) {
     this.x = _x;
@@ -29,15 +35,39 @@ class Ball {
   move() {
 
     if (this.pos.x >= width || this.pos.x <= 0) {
+
+      if (this.vel.x < 0){
+        this.vel.x += 2 * this.accel.x;
+      }
+      else{
+        this.vel.x -= 2 * this.accel.x;
+      }
       this.vel.x *= -1;
 
       //this.accel.x *= 0.1;
     }
     if (this.pos.y >= height || this.pos.y <= 0) {
+      if (this.vel.y < 0){
+        this.vel.y += 2 * this.accel.y;
+      }
+      else{
+        this.vel.y -= 2 * this.accel.y;
+      }
       this.vel.y *= -1
-      print(this.vel.y);
 
       //this.accel.y *= 0.1;
+    }
+    if (this.pos.x < 0){
+      this.pos.x = 1;
+    }
+    if (this.pos.x > width){
+      this.pos.x = width -1;
+    }
+    if (this.pos.y < 0){
+      this.pos.y = 1;
+    }
+    if (this.pos.y > height){
+      this.pos.y = height - 1;
     }
     this.pos.add(this.vel);
     this.vel.add(this.accel);
