1289a1a6d44e637eff06b8c938972efaf8b263b5
alexpohorecky <alexanderpohorecky@gmail.com>
2018-03-14 15:20:48 -0600

    idfk

--- translationDemo/sketch.js
+++ translationDemo/sketch.js
@@ -1,22 +1,39 @@
+let clockSize;
 function setup(){
+  createCanvas(windowWidth, windowHeight);
   if (windowHeight < windowWidth){
-    createCanvas(windowHeight, windowHeight);
+    clockSize = windowHeight * 0.9;
   }
   else{
-    createCanvas(windowWidth,windowWidth);
+    clockSize = windowWidth * 0.9;
   }
 }
 
 function draw(){
   background(255);
+  push();
   translate(width/2,height/2);
   angleMode(DEGREES);
-  ellipse(0,0,width);
-  for (let i = 0; i < 360; i++){
-    if (i % 5 === 0){
-      rotate(i);
-      rectMode(CENTER)
-      rect(0, 0, 5, 20);
-    }
+  strokeWeight(8);
+  fill(255);
+  ellipse(0,0,clockSize);
+  fill(0);
+  ellipse(0,0,4);
+
+  for (let i = 0; i < 12; i++){
+  rotate(360/12);
+  fill(0);
+  strokeWeight(10);
+  strokeCap(SQUARE);
+  line(0, clockSize/2 * 0.95, 0, clockSize/2 * 0.75)
   }
+
+  for (let i = 0; i < 60; i++){
+  rotate(360/60);
+  fill(0);
+  strokeWeight(4);
+  strokeCap(SQUARE);
+  line(0, clockSize/2 * 0.95, 0, clockSize/2 * 0.8)
+  }
+  pop();
 }
