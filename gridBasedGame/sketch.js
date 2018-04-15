fab20874c2a252d65824437f42d47001d599c96d
ComorantPostman <alexanderpohorecky@gmail.com>
2018-04-15 17:38:32 -0600

    Have the monimo almost working

--- gridBasedGame/sketch.js
+++ gridBasedGame/sketch.js
@@ -1,55 +1,102 @@
-let dropSpeed;
-let level;
-let grid;
-let pieceSize = 25;
+let startPosition = {
+  x: 4,
+  y: 0,
+}
+let player = {
+  x: startPosition.x,
+  y: startPosition.y,
+  value: 1,
+
+}
+
+let arena;
+let arenaWidth = 10;
+let arenaHeight = 20;
+
+let monimoSize = 25;
+
+let hangTime = 1000;
+let time = 0;
+let deltaTime;
+
+
 function setup(){
-  createCanvas(windowWidth, windowHeight);
-  grid = new Grid(10, 20);
-  grid.drawGrid();
+  createCanvas(arenaWidth * monimoSize, arenaHeight * monimoSize);
+
+  arena = create2DArray(arenaWidth, arenaHeight);
+  spawnPlayer();
 
 }
 
 function draw(){
-
-}
-
-function create2DArray(arrayWidth, arrayHeight){
-  let array2D = new Array(arrayWidth);
-  for (let i = 0; i < array2D.length; i++){
-    array2D[i] = new Array(arrayHeight);
+  background(200);
+  showArena();
+  showPlayer();
+  deltaTime = millis();
+  if (deltaTime - time >= hangTime){
+    dropPlayer();
+    time = deltaTime;
   }
-  return array2D;
-}
-
-function drawGrid(){
 
 }
 
-class Grid {
-  constructor(_w, _h){
-    this.w = _w;
-    this.h = _h;
-    this.cellSize = pieceSize;
-    this.gridArray = create2DArray(this.w, this.h);
+function keyPressed(){
+  if (keyCode === 37){
+    player.x--;
+    if (player.x < 0){
+      player.x = 0;
+    }
 
   }
-  drawGrid(){
-    for (let i = 0; i < this.w; i++){
-      for (let j = 0; j < this.h; j++){
-        rect(i * this.cellSize, j * this.cellSize, this.cellSize, this.cellSize);
+  if (keyCode === 39){
+    player.x++;
+    if (player.x >= arenaWidth){
+      player.x = arenaWidth - 1;
+    }
+  }
+  if (keyCode === 40){
+    dropPlayer();
+  }
+}
+
+function create2DArray(gridWidth, gridHeight){
+  let grid = Array(gridWidth);
+  for (let i = 0; i < gridWidth; i++){
+    grid[i] = Array(gridHeight);
+    grid[i].fill(0);
+  }
+
+  return grid;
+}
+
+function spawnPlayer(){
+  player.x = startPosition.x;
+  player.y = startPosition.y;
+}
+
+function showPlayer(){
+  fill(0);
+  rect(player.x * monimoSize, player.y * monimoSize, monimoSize, monimoSize);
+}
+
+function dropPlayer(){
+  player.y++;
+  if (player.y === arenaHeight){
+    placePiece(arena, player);
+  }
+}
+
+function placePiece(arena, player){
+  arena[player.x][player.y] = player.value;
+}
+
+function showArena(){
+  for (let i = 0; i < arenaWidth; i++){
+    for (let j = 0; j < arenaHeight; j++){
+      if (arena[i][j] === 1){
+        fill(0);
+        rect(i * monimoSize, j * monimoSize, monimoSize, monimoSize);
       }
     }
   }
-
-}
-class Monimo {
-  constructor(startingX, startingY){
-    this.x = startingX;
-    this.y = startingY;
-
-  }
-  dropDown(){
-    this.y++;
-  }
-
 }
