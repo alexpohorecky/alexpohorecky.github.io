2c7a08d597e35a416193717b8bc24365d45f9d72
ComorantPostman <alexanderpohorecky@gmail.com>
2018-04-18 00:02:09 -0600

    yay, finished i think...

--- gridBasedGame/sketch.js
+++ gridBasedGame/sketch.js
@@ -1,5 +1,21 @@
+/*
+Alex Pohorecky
+April 17, 2018
+
+Connect Fourtris
+
+The goal of the game is to create stacks of 4 blocks of the same color.
+Use the left and right arrow keys to move the falling block left and right respectively.
+Use the down arrow key to skip down a row.
+When you stack 4 identical blocks successfully, they disappear and drop down every other block in the column.
+Each time a stack is cleared, you gain 1 point and the speed at which the blocks fall increases.
+Once the blocks reach the top of the arena, the arena clears, the speed resets, and your points are set to 0.
+
+Sorry for the checkArena function being so bad. I had a lot of trouble with it, and when I found something that worked I kinda just stuck with it.
+
+*/
 let startPosition = {
-  x: 4,
+  x: 2,
   y: 0,
 }
 let player = {
@@ -10,18 +26,22 @@ let player = {
 }
 
 let arena;
-let arenaWidth = 10;
-let arenaHeight = 10;
+let arenaWidth = 5;
+let arenaHeight = 20;
 
 let monimoSize = 25;
 
 let hangTime = 500;
+let score = 0;
+let highScore = 0;
+
 let time = 0;
 let deltaTime;
 
 
 function setup(){
-  createCanvas(arenaWidth * monimoSize, arenaHeight * monimoSize);
+  createCanvas(displayWidth, displayHeight);
+  document.documentElement.style.overflow = 'hidden';
 
   arena = create2DArray(arenaWidth, arenaHeight);
   spawnPlayer();
@@ -29,6 +49,11 @@ function setup(){
 }
 
 function draw(){
+  background(255)
+  fill(0);
+  text("High Score: " + highScore, 150, 10);
+  text("Score: " + score, 150, 25);
+  text("The goal of the game is to create stacks of 4 blocks of the same color.\nUse the left and right arrow keys to move the falling block left and right respectively.\nUse the down arrow key to skip down a row.", 150, 40)
   showArena();
   showPlayer();
   deltaTime = millis();
@@ -78,6 +103,14 @@ function spawnPlayer(){
   player.x = startPosition.x;
   player.y = startPosition.y;
   player.value = ceil(random(0,7));
+  if (arena[player.x][player.y] != 0){
+    arena = create2DArray(arenaWidth, arenaHeight);
+    if (score > highScore){
+      highScore = score;
+    }
+    score = 0;
+    hangTime = 500;
+  }
 }
 
 function showPlayer(){
@@ -90,18 +123,18 @@ function dropPlayer(){
   if (player.y === arenaHeight){
     placePiece(arena, player);
     spawnPlayer();
-    hangTime *= 0.99;
   }
   if (arena[player.x][player.y] > 0){
       placePiece(arena, player);
       spawnPlayer();
-      hangTime *= 0.99;
   }
-  checkArena();
+
 }
 
 function placePiece(arena, player){
   arena[player.x][player.y-1] = player.value;
+  checkArena();
+
 }
 
 function showArena(){
@@ -113,70 +146,26 @@ function showArena(){
   }
 }
 
-// function checkArena(){
-//   for (let i = 0; i < arenaWidth; i++){
-//     for (let j = 0; j < arenaHeight; j++){
-//       let pieceValue = arena[i][j];
-//       let pieceStack =[[i,j]];
-//       let lastPiece = pieceStack[pieceStack.length - 1];
-//       for(let x = -1; x <= 1; x++){
-//         let currentPieceCoord = [i+x, j];
-//         if (!existInArray(currentPieceCoord, pieceStack) && arena[i+x,j] === pieceValue){
-//           pieceStack.push(currentPieceCoord);
-//         }
-//       }
-//       for(let y = -1; y <= 1; y++){
-//         let currentPieceCoord = [i, j + y];
-//         if (!existInArray(currentPieceCoord, pieceStack) && arena[i,j+y] === pieceValue){
-//           pieceStack.push(currentPieceCoord);
-//         }
-//       }
-//       if (pieceStack.length >= 4){
-//         for(let coord of pieceStack){
-//           arena[coord[0]].splice(coord[1], 1);
-//           arena[coord[0]].unshift(0);
-//         }
-//       }
-//     }
-//   }
-// }
-
 function checkArena(){
   for (let i = 0; i < arenaWidth; i++){
     for (let j = 0; j < arenaHeight; j++){
-      checkNeighbours(i, j, arena[i][j]);
-
-    }
-  }
-}
-
-function checkNeighbours(currentX, currentY, currentValue){
-  let neighbourCoords = []
-  for (let x = -1; x <= 1; x++){
-    currentNeighbourCoord = [currentX + x,currentY];
-    if ( currentNeighbourCoord === currentValue){
-      if (!existInArray(currentNeighbourCoord, neighbours)){
-        neighbourCoords.push(currentNeighbourCoord);
+      let pieceValue = arena[i][j];
+      if(pieceValue > 0){
+        if(arena[i][j-1] === pieceValue){
+          if(arena[i][j-2] === pieceValue){
+            if(arena[i][j-3] === pieceValue){
+              arena[i].splice(j-3, 4);
+              arena[i].unshift(0,0,0,0);
+              hangTime *= 0.95;
+              score+=1;
+            }
+          }
+        }
       }
     }
   }
-  for (let y = -1; y <= 1; y++){
-    currentNeighbourCoord = [currentX,currentY + y];
-    if ( currentNeighbourCoord === currentValue){
-      if (!existInArray(currentNeighbourCoord, neighbours)){
-        neighbourCoords.push(currentNeighbourCoord);
-      }
-    }
-  }
-  for (let coord of neighbourCoords){
-    removeCoords(coord);
-  }
 }
 
-function removeCoords(coord){
-  arena[coord[0]].splice(coord[1], 1);
-  arena[coord[0]].unshift(0);
-}
 
 function existInArray(item, array){
   for(let i = 0; i < array.length; i++){
@@ -189,6 +178,6 @@ function existInArray(item, array){
 
 
 function fillMonimo(value){
-  colorChoice = [200, "Red", "Green", "Blue", "Yellow", "Orange", "Purple", "Pink"];
+  colorChoice = [150, "Red", "Green", "Blue", "Yellow", "Orange", "Purple", "Pink"];
   fill(colorChoice[value]);
 }
