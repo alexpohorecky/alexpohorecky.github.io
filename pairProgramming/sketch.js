17173aa01262b9a83091a441a41d70435e5e3ae6
alexpohorecky <alexanderpohorecky@gmail.com>
2018-04-27 15:20:20 -0600

    ahhhhhhhhhh

--- pairProgramming/sketch.js
+++ pairProgramming/sketch.js
@@ -1,5 +1,6 @@
 let backgroundGraphic;
 let cannon;
+let mouseXPos;
 function preload(){
   backgroundGraphic = loadImage('peggleBaseScreen.png');
   cannon = loadImage('peggleCannon.png');
@@ -9,6 +10,7 @@ function preload(){
 function setup(){
   createCanvas(windowWidth, windowHeight);
   document.documentElement.style.overflow = 'hidden';
+  document.body.style.cursor = 'none';
 
   image(backgroundGraphic,0,0, width, height);
 
@@ -16,9 +18,20 @@ function setup(){
 }
 
 function draw(){
+  imageMode(CORNER);
+  image(backgroundGraphic,0,0, width, height);
   translate(width/2, 0);
   fill("green");
   strokeWeight(5);
-  rectMode(CENTER);
-  rect(0,0,20,60);
+  angleMode(DEGREES);
+  rotate(map(constrain(mouseX,104,590), 590, 104, -90, 90));
+  imageMode(CENTER);
+  image(cannon,0,0,80,50);
+
+  // rectMode(CENTER);
+  // rect(0,0,20,60);
+}
+
+function mouseClicked(){
+  mouseXPos = mouseX;
 }
