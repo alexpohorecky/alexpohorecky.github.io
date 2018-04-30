f7ad5f769be6807c6c7340d7d062a6501e12d5ca
alexpohorecky <alexanderpohorecky@gmail.com>
2018-04-30 15:20:33 -0600

    hmmm

--- pairProgramming/ball.js
+++ pairProgramming/ball.js
@@ -9,43 +9,42 @@ class Ball {
 
     this.pos = createVector(this.x, this.y);
     this.vel = createVector(xVelocity, yVelocity);
-    this.unitNormalVel;
-    this.unitTangentVel;
     this.accel = createVector(xAcceleration, yAcceleration);
+    this.vel.rotate(map(constrain(mouseX,104,590), 590, 104, -90, 90));
   }
   move() {
     // If a ball hits the wall...
-    if (this.pos.x > width - this.r || this.pos.x < 0 + this.r) {
-      // Ball bounces
-      this.vel.x *= -1;
-
-    }
+    // if (this.pos.x > width - this.r || this.pos.x < 0 + this.r) {
+    //   // Ball bounces
+    //   this.vel.x *= -1;
+    //
+    // }
     // If a ball hits the floor or ceiling...
     if (this.pos.y > height - this.r || this.pos.y < 0 + this.r) {
       // Lose vertical speed due to not perfectly ellastic collision
       if (this.vel.y < 0) {
-        this.vel.y += elasticity * this.accel.y;
+        this.vel.y += 3 * this.accel.y;
       }
       else {
-        this.vel.y -= elasticity * this.accel.y;
+        this.vel.y -= 3 * this.accel.y;
       }
       // Lose horizontal speed due to friction
       if (this.vel.x < 0) {
-        this.vel.x += -1 * friction * this.vel.x;
+        this.vel.x += -1 * 0.05 * this.vel.x;
       }
       else {
-        this.vel.x -= friction * this.vel.x;
+        this.vel.x -= 0.05 * this.vel.x;
       }
       // Ball bounces
       this.vel.y *= -1
     }
     // Ball bounces on its edge
-    if (this.pos.x < 0 + this.r) {
-      this.pos.x = 0 + this.r;
-    }
-    if (this.pos.x > width - this.r) {
-      this.pos.x = width - this.r;
-    }
+    // if (this.pos.x < 0 + this.r) {
+    //   this.pos.x = 0 + this.r;
+    // }
+    // if (this.pos.x > width - this.r) {
+    //   this.pos.x = width - this.r;
+    // }
     if (this.pos.y < 0 + this.r) {
       this.pos.y = 0 + this.r;
     }
@@ -56,8 +55,6 @@ class Ball {
     this.pos.add(this.vel);
     // Acceleration affects velocity
     this.vel.add(this.accel);
-
-    this.unitNormalVel = 
   }
   animate() {
     fill(this.clrR, this.clrG, this.clrB);
--- pairProgramming/sketch.js
+++ pairProgramming/sketch.js
@@ -1,6 +1,7 @@
 let backgroundGraphic;
 let cannon;
 let mouseXPos;
+let balls = [];
 function preload(){
   backgroundGraphic = loadImage('peggleBaseScreen.png');
   cannon = loadImage('peggleCannon.png');
@@ -8,7 +9,7 @@ function preload(){
 }
 
 function setup(){
-  createCanvas(1280, 800);
+  createCanvas(windowWidth, windowHeight);
   document.documentElement.style.overflow = 'hidden';
   document.body.style.cursor = 'none';
 
@@ -20,18 +21,26 @@ function setup(){
 function draw(){
   imageMode(CORNER);
   image(backgroundGraphic,0,0, width, height);
-  translate(width/2, 0);
+
   fill("green");
   strokeWeight(5);
+    imageMode(CENTER);
+  image(cannon,width/2,5,80,50);
+  translate(width/2, 0);
   angleMode(DEGREES);
+  push();
   rotate(map(constrain(mouseX,104,590), 590, 104, -90, 90));
-  imageMode(CENTER);
-  image(cannon,0,0,80,50);
 
+
+  pop();
+  for (let ball of balls){
+    ball.move();
+    ball.animate();
+  }
   // rectMode(CENTER);
   // rect(0,0,20,60);
 }
 
 function mouseClicked(){
-  mouseXPos = mouseX;
+    balls.push(new Ball(0,0,10,0,5,0,0,0,0,0));
 }
