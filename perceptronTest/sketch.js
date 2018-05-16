5152437c1f44095b576ea806c7fea01c14debeca
alexpohorecky <alexanderpohorecky@gmail.com>
2018-05-16 11:04:36 -0600

    almost finne

--- perceptronTest/sketch.js
+++ perceptronTest/sketch.js
@@ -9,7 +9,19 @@ function draw(){
 }
 
 class Perceptron {
-  constructor(){
+  constructor(numOfWeights, bias){
+    this.weights = [];
+    for (let i = 0; i < numOfWeights; i++){
+      this.weights.push(random(-1,1));
+    }
+
+  }
+  this.feedForward(inputs){
+    this.sum = 0;
+    for (let i = 0; i < inputs.length; i++){
+      this.sum += inputs[i]*this.weights[i];
+
+    }
 
   }
 }
