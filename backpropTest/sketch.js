72219c7697ccc7b6474e2dccaebfab9f9349e3b3
ComorantPostman <alexanderpohorecky@gmail.com>
2018-06-12 16:47:17 -0600

    welp i've given up on the xor, no time
    gonna skip straight to handwriting recognition

--- backpropTest/sketch.js
+++ backpropTest/sketch.js
@@ -4,14 +4,14 @@ First Crack At Back Propagation
 Gonna Try solving the XOR problem
 Time to see if I know what I'm doing...
 */
-let examples = [[1,0]];
+//let examples = [[1,0]];
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  trainer = new Trainer(examples);
-  network = new Network([2,2,1], trainer.answers);
+  //trainer = new Trainer(examples);
+  network = new Network([2,2,1]);
   network.addNodes();
   network.generateWeights();
-  network.feedForward(examples[0][0],examples[0][1]);
+  network.feedForward(1,0);
 }
 
 function draw(){
@@ -37,6 +37,7 @@ function outputError(outputActivation, answer, weightedSum){
 
 
 
+
 }
 class Trainer{
   constructor(examples){
@@ -52,9 +53,10 @@ class Trainer{
   }
 }
 class Network{
-  constructor(_networkOutline, answers){
+  constructor(_networkOutline){
     this.networkOutline = _networkOutline;
     this.networkMatrix = [];
+    this.currentInput;
 
   }
   addNodes(){
@@ -77,6 +79,7 @@ class Network{
   }
 
   feedForward(a,b){
+    this.currentInput = [a,b];
     this.networkMatrix[0][0].activation = sigmoid(a+this.networkMatrix[0][0].bias);
     this.networkMatrix[0][1].activation = sigmoid(b+this.networkMatrix[0][1].bias);
 
@@ -95,17 +98,29 @@ class Network{
 
 
   shiftBackError(){
-    this.networkMatrix[this.networkMatrix.length-1][0].error = outputError(this.networkMatrix[this.networkMatrix.length-1][0].activation, answers[0], this.networkMatrix[this.networkMatrix.length-1][0].weightedSum);
-    let weightMatrix = [];
-    let errorVector = [];
-    let weightedSumVector = [];
-    for (let node = 0; node < this.networkMatrix[layerIndex+1].length; node++){
-      let nodeWeights = [];
-      for (let weight = 0; weight < this.networkMatrix[layerIndex+1][node].weights.length; weight++){
-        nodeWeights.push(this.networkMatrix[layerIndex+1][node].weights[weight]);
+    this.networkMatrix[this.networkMatrix.length-1][0].error = outputError(this.networkMatrix[this.networkMatrix.length-1][0].activation, this.currentInput[0]^this.currentInput[1], this.networkMatrix[this.networkMatrix.length-1][0].weightedSum);
+    for (let layerIndex = this.networkMatrix.length-1; layerIndex = 0; layerIndex--){
+      let weightMatrix = [];
+      let weightedSumVector = [];
+      let errorVector = [];
+      if (layerIndex === this.networkMatrix.length-1){
+        errorVector = [this.networkMatrix[this.networkMatrix.length-1][0].error];
       }
-      weightMatrix.push(nodeWeights);
-    }
+      else{
+        for (let node = 0; node < this.networkMatrix[layerIndex+1].length; node++){
+          errorVector.push(this.networkMatrix[layerIndex+1][node].error);
+        }
+      }
+      for (let node = 0; node < this.networkMatrix[layerIndex+1].length; node++){
+        let nodeWeights = [];
+        for (let weight = 0; weight < this.networkMatrix[layerIndex+1][node].weights.length; weight++){
+          nodeWeights.push(this.networkMatrix[layerIndex+1][node].weights[weight]);
+        }
+        weightMatrix.push(nodeWeights);
+      }
+      for (let node = 0; node < this.networkMatrix[layerIndex].length; node++){
+        weightedSumVector.push(this.networkMatrix[layerIndex][node].weightedSum);
+      }
 
   }
 
